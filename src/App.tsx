afc859a4493491c16d4a55b57ede4e6d6ce876b5
psh1124 <a01038818368@gmail.com>
2026-01-16 17:32:14 +0900

    React 복습

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
