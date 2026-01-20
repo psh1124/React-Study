3a0179b79e1acc52c258ccaa57e3199fd30b039e
psh1124 <a01038818368@gmail.com>
2026-01-20 17:52:12 +0900

    fix: InputField 컴포넌트 개선

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,29 +1,37 @@
 import "./InputField.css";
 
 interface InputFieldProps {
-  id: string;
+  id?: string;
+  name?: string;
+  className: string;
   type?: string;
   placeholder?: string;
   value: string;
+  autocomplete?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur?: () => void;
 }
 
 function InputField({
   id,
+  name,
+  className,
   type = "text",
   placeholder,
   value,
+  autocomplete,
   onChange,
   onBlur,
 }: InputFieldProps) {
   return (
     <input
       id={id}
-      className="input-field"
+      name={name}
+      className={`input-field ${className}`}
       type={type}
       placeholder={placeholder}
       value={value}
+      autoComplete={autocomplete}
       onChange={onChange}
       onBlur={onBlur}
     />
