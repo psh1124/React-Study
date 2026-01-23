8a4796836de953a047dc038f8fda542f0ff1eb5a
psh1124 <a01038818368@gmail.com>
2026-01-23 17:38:48 +0900

    ui: 홈 화면에서 로그인 사용자 정보 사용 구조 정리

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,19 @@
 import Container from "../../components/Container/Container";
 import Card from "../../components/Card/Card";
-import { useState } from "react";
+import { useAuth } from "../../context/useAuth";
 
 function Home() {
-  const [email] = useState("");
-  console.log({email})
+  const { user, isLoggedIn } = useAuth();
+  console.log(user);
+
   return (
     <Container>
       <h1>Home Page</h1>
+
+      {isLoggedIn && user && (
+        <p>{user.nickname}님 환영합니다.</p>
+      )}
+
       <div>
         <Card title="섹션 1" content="내용 예시 1" section="섹션 1"></Card>
         <Card title="섹션 2" content="내용 예시 2" section="섹션 2"></Card>
