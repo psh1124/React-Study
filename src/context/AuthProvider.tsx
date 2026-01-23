351cd430cccc06183d8337db3793ff701948d842
psh1124 <a01038818368@gmail.com>
2026-01-23 17:38:29 +0900

    auth: Context 기반 로그인 상태 관리 및 localStorage 연동

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import { AuthContext, type AuthContextType, type User } from "./AuthContext";
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const login = (user: User) => {
     setUser(user);
+    localStorage.setItem("user", JSON.stringify(user));
   };
 
   const logout = () => {
     setUser(null);
+    localStorage.removeItem("user")
   };
 
   const value: AuthContextType = {
