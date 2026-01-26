3103c81a746ac045cb02f123349ea14d5281068a
psh1124 <a01038818368@gmail.com>
2026-01-26 17:57:50 +0900

    refactor: 라우팅 구조 및 인증 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Home from "./pages/home/Home"
-import Login from "./pages/login/Login"
-import Signup from "./pages/signup/Signup"
-import { AuthProvider } from './context/AuthProvider'
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/home/Home";
+import Login from "./pages/Auth/login/Login";
+import Signup from "./pages/Auth/signup/Signup";
+import MyPage from "./pages/mypage/MyPage";
+import PublicRoute from "./components/Route/PublicRoute";
+import ProtectedRoute from "./components/Route/ProtectedRoute";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
   return (
@@ -12,12 +18,49 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
+
+          {/* 로그인 X */}
+          <Route
+            path="login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="signup"
+            element={
+              <PublicRoute>
+                <Signup />
+              </PublicRoute>
+            }
+          />
+
+          {/* 로그인 O */}
+          <Route
+            path="mypage"
+            element={
+              <ProtectedRoute>
+                <MyPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
+
+        <ToastContainer
+          style={{ top: "calc(var(--header-height) + 30px)" }}
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick
+          draggable
+          theme="dark"
+          transition={Slide}
+        />
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
