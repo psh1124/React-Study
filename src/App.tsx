48698e6e13876e0b21ff1228757d8daeea9eafe8
psh1124 <a01038818368@gmail.com>
2026-01-28 17:27:05 +0900

    feat: ErrorBoundary 및 LoadingProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer, Slide } from "react-toastify";
+import { LoadingProvider } from "./context/LoadingProvider";
 import Header from "./components/Header/Header";
 import Home from "./pages/home/Home";
 import Login from "./pages/Auth/login/Login";
@@ -7,59 +9,63 @@ import Signup from "./pages/Auth/signup/Signup";
 import MyPage from "./pages/mypage/MyPage";
 import PublicRoute from "./components/Route/PublicRoute";
 import ProtectedRoute from "./components/Route/ProtectedRoute";
-import { ToastContainer, Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import "./App.css";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
+    <ErrorBoundary>
+      <AuthProvider>
+        <LoadingProvider>
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
 
-          {/* 로그인 X */}
-          <Route
-            path="login"
-            element={
-              <PublicRoute>
-                <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="signup"
-            element={
-              <PublicRoute>
-                <Signup />
-              </PublicRoute>
-            }
-          />
+              {/* 로그인 X */}
+              <Route
+                path="login"
+                element={
+                  <PublicRoute>
+                    <Login />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="signup"
+                element={
+                  <PublicRoute>
+                    <Signup />
+                  </PublicRoute>
+                }
+              />
 
-          {/* 로그인 O */}
-          <Route
-            path="mypage"
-            element={
-              <ProtectedRoute>
-                <MyPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+              {/* 로그인 O */}
+              <Route
+                path="mypage"
+                element={
+                  <ProtectedRoute>
+                    <MyPage />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
 
-        <ToastContainer
-          style={{ top: "calc(var(--header-height) + 30px)" }}
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          closeOnClick
-          draggable
-          theme="dark"
-          transition={Slide}
-        />
-      </BrowserRouter>
-    </AuthProvider>
+            <ToastContainer
+              style={{ top: "calc(var(--header-height) + 30px)" }}
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              closeOnClick
+              draggable
+              theme="dark"
+              transition={Slide}
+            />
+          </BrowserRouter>
+        </LoadingProvider>
+      </AuthProvider>
+    </ErrorBoundary>
   );
 }
 
